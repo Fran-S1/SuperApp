2fe3b4514099c49f68b3c311069ae4bab9f45315
Fran-S1 <franmanu04@outlook.com>
2025-07-21 16:52:13 -0300

    Add files via upload

--- 3_calculadoraDefinitiva.js
+++ 3_calculadoraDefinitiva.js
@@ -2,118 +2,122 @@ const agregarItem = document.getElementById("agregar-item")
 const nombreItem = document.getElementById("escribir-item")
 const listaItems = document.getElementById("lista-items")
 
+// FUNCIONES //
+function formarLi (){
+    const li = document.createElement("li")
+    const item = document.createElement("span")
+    item.textContent = nombreItem.value
+    item.classList.add("pruebaTexto")
+
+    const cantItems = document.createElement("span")
+    cantItems.textContent = "1"
+    cantItems.classList.add("cant-items")
+
+    const btnSumar = document.createElement("button")
+    btnSumar.classList.add("sumar", "actualizarTotalReSum")
+    btnSumar.addEventListener("click",()=>{
+        cantItems.textContent = Number(cantItems.textContent) + 1
+    })
+    const btnRestar = document.createElement("button")
+    btnRestar.classList.add("restar","actualizarTotalReSum")
+    btnRestar.addEventListener("click",()=>{
+        if (cantItems.textContent >= 1){
+            cantItems.textContent = Number(cantItems.textContent) - 1
+        }
+        if (cantItems.textContent == 0){
+            const ul = document.querySelector("ul")
+            ul.removeChild(li)
+        }
+    })
+
+    const precioItem = document.createElement("input")
+    precioItem.type="number"
+    precioItem.classList.add("precioItem", "inputsCalculadora", "actualizarTotalInp")
+
+    const cantDesc = document.createElement("input")
+    cantDesc.type="number"
+    cantDesc.classList.add("cant-desc", "inputsCalculadora", "actualizarTotalInp")
+
+    // SIMBOLOS //
+    const simboloPrecio = document.createElement("span")
+    simboloPrecio.classList.add("simboloPrecio")
+
+    const simboloDescuento = document.createElement("span")
+    simboloDescuento.classList.add("simboloDescuento")
+
+    //  CREAR LI //
+    li.appendChild(btnRestar)
+    li.appendChild(cantItems)
+    li.appendChild(btnSumar)
+    li.appendChild(item)
+
+    li.appendChild(simboloPrecio)
+    li.appendChild(precioItem)
+    li.appendChild(simboloDescuento)
+    li.appendChild(cantDesc)
+
+    listaItems.appendChild(li)
+    nombreItem.value = ""
+}
+function calcularTotal (){
+    const total = document.getElementById("total")
+    let totalLista = 0
+
+    const lis = document.querySelectorAll("li")
+    const allCantItems = document.querySelectorAll(".cant-items")
+    const allPrecios = document.querySelectorAll(".precioItem")
+    const allDescuentos = document.querySelectorAll(".cant-desc")
+    total.textContent = "0"
+
+    for(i=0; i<lis.length; i++){
+        const CantItemNumerico = Number(allCantItems[i].textContent)
+        const PrecioNumerico = Number(allPrecios[i].value)
+        const cantPorPrecio = CantItemNumerico * PrecioNumerico
+        const DescuentoNumerico = Number(allDescuentos[i].value)
+
+        const preSubtotal = cantPorPrecio * (DescuentoNumerico / 100)
+        const subtotal = cantPorPrecio - preSubtotal
+
+        totalLista += subtotal
+    }
+    let mostrarTotal = Number(total.textContent) + totalLista
+    totalLista = 0
+    total.textContent = "TOTAL: $" + mostrarTotal
+}
+
 nombreItem.addEventListener("keydown",(teclaPresionada)=>{
-    if (teclaPresionada.key === "Enter"){
-        const li = document.createElement("li")
-        const item = document.createTextNode(nombreItem.value)
+    if (teclaPresionada.key === "Enter" && nombreItem.value != ""){
 
-        const cantItems = document.createElement("span")
-        cantItems.textContent = "1"
-        cantItems.classList.add("cant-items")
+        formarLi()
 
-        const btnSumar = document.createElement("button")
-        btnSumar.classList.add("sumar", "actualizarTotalResum")
-        btnSumar.addEventListener("click",()=>{
-            cantItems.textContent = Number(cantItems.textContent) + 1
-        })
-        const btnRestar = document.createElement("button")
-        btnRestar.classList.add("restar","actualizarTotalResum")
-        btnRestar.addEventListener("click",()=>{
-            if (cantItems.textContent >= 1){
-                cantItems.textContent = Number(cantItems.textContent) - 1
-            }
-            if (cantItems.textContent == 0){
-                const ul = document.querySelector("ul")
-                ul.removeChild(li)
-            }
-        })
-        
-        const precioItem = document.createElement("input")
-        precioItem.type="number"
-        precioItem.classList.add("precioItem", "inputsCalculadora", "actualizarTotalInp")
-
-        const cantDesc = document.createElement("input")
-        cantDesc.type="number"
-        cantDesc.classList.add("cant-desc", "inputsCalculadora", "actualizarTotalInp")
-
-        // SIGNOS //
-        const signoPrecio = document.createElement("span")
-        signoPrecio.classList.add("signoPrecio")
-
-        const signoDescuento = document.createElement("span")
-        signoDescuento.classList.add("signoDescuento")
-
-        //  AGREGAR ITEM Y COMPLEMENTOS //
-        li.appendChild(btnRestar)
-        li.appendChild(cantItems)
-        li.appendChild(btnSumar)
-        li.appendChild(item)
-
-        li.appendChild(signoPrecio)
-        li.appendChild(precioItem)
-        li.appendChild(signoDescuento)
-        li.appendChild(cantDesc)
-
-        listaItems.appendChild(li)
-        nombreItem.value = ""
-
-        ////
-        const actualizarLista = document.getElementById("actualizarLista")
-        const total = document.getElementById("total")
-        let totalLista = 0
-
-        document.querySelectorAll(".actualizarTotalResum").forEach(boton =>{
+        document.querySelectorAll(".actualizarTotalReSum").forEach(boton =>{
             boton.addEventListener("click",()=>{
-                const lis = document.querySelectorAll("li")
-                const allCantItems = document.querySelectorAll(".cant-items")
-                const allPrecios = document.querySelectorAll(".precioItem")
-                const allDescuentos = document.querySelectorAll(".cant-desc")
-                total.textContent = "0"
-
-                for(i=0; i<lis.length; i++){
-                    const CantItemNumerico = Number(allCantItems[i].textContent)
-                    const PrecioNumerico = Number(allPrecios[i].value)
-                    const cantPorPrecio = CantItemNumerico * PrecioNumerico
-                    const DescuentoNumerico = Number(allDescuentos[i].value)
-
-                    const preSubtotal = cantPorPrecio * (DescuentoNumerico / 100)
-                    const subtotal = cantPorPrecio - preSubtotal
-
-                    totalLista += subtotal
-                }
-                let mostrarTotal = Number(total.textContent) + totalLista
-                totalLista = 0
-                total.textContent = "TOTAL: $" + mostrarTotal
-            
+                calcularTotal()
             })
         })
         document.querySelectorAll(".actualizarTotalInp").forEach(input =>{
             input.addEventListener("input",()=>{
-                const lis = document.querySelectorAll("li")
-                const allCantItems = document.querySelectorAll(".cant-items")
-                const allPrecios = document.querySelectorAll(".precioItem")
-                const allDescuentos = document.querySelectorAll(".cant-desc")
-                total.textContent = "0"
-
-                for(i=0; i<lis.length; i++){
-                    const CantItemNumerico = Number(allCantItems[i].textContent)
-                    const PrecioNumerico = Number(allPrecios[i].value)
-                    const cantPorPrecio = CantItemNumerico * PrecioNumerico
-                    const DescuentoNumerico = Number(allDescuentos[i].value)
-
-                    const preSubtotal = cantPorPrecio * (DescuentoNumerico / 100)
-                    const subtotal = cantPorPrecio - preSubtotal
-
-                    totalLista += subtotal
-                }
-                let mostrarTotal = Number(total.textContent) + totalLista
-                totalLista = 0
-                total.textContent = "TOTAL: $" + mostrarTotal
-            
+                calcularTotal()
             })
         })
     }
+})
 
+agregarItem.addEventListener("click",()=>{
+    if (nombreItem.value != ""){
+        formarLi()
+
+        document.querySelectorAll(".actualizarTotalReSum").forEach(boton =>{
+            boton.addEventListener("click",()=>{
+                calcularTotal()
+            })
+        })
+        document.querySelectorAll(".actualizarTotalInp").forEach(input =>{
+            input.addEventListener("input",()=>{
+                calcularTotal()
+            })
+        })
+    }
 })
 
 
